d5866cdecaaefa7ce54ef867e9b499f184cab02b
longthien123 <longthienl80@gmail.com>
2025-11-22 17:20:48 +0700

    fix link apibase 2

--- frontend/src/utils/AxiosCustom.js
+++ frontend/src/utils/AxiosCustom.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import nProgress from "nprogress";
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api/';
+const API_BASE_URL = process.env.REACT_APP_API_URL ;
 const instance = axios.create({
     baseURL: API_BASE_URL,
     // timeout: 1000,
