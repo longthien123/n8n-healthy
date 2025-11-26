e189cce618701c1bfd404e33ba16f6807d667aa0
longthien123 <longthienl80@gmail.com>
2025-11-27 00:27:42 +0700

    feature/send_doctors

--- backend/n8n/urls.py
+++ backend/n8n/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("book/", views.send_booking_to_n8n, name="send_booking_to_n8n"),
     path('cron-check/', views.trigger_reminders_view, name='trigger_reminders'),
+    path('doctor-sends/', views.doctors_reminders_view, name='doctors_reminders'),
 ]
